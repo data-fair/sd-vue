69bad5afa11ef13e4db4dc03ac52d0b2bda16c8e
Alban Mouton <alban.mouton@gmail.com>
2022-05-23 17:22:56 +0200

    fix: small visual problem with lang-sitcher border

--- src/vuetify/lang-switcher.vue
+++ src/vuetify/lang-switcher.vue
@@ -8,6 +8,7 @@
     <template #activator>
       <v-btn
         depressed
+        color="transparent"
         style="height: 100% !important;border-radius: 0;"
       >
         {{ $i18n.locale }}
