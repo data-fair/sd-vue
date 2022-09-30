0cb4826bcf4cef8963c3365c76f941604719ecf0
Alban Mouton <alban.mouton@gmail.com>
2022-09-30 16:51:19 +0200

    feat: show department avatar

--- src/vuetify/avatar.vue
+++ src/vuetify/avatar.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="sd-avatar" :class="{'has-org-avatar': showAccount && activeAccount.type !== 'user'}">
+  <div class="sd-avatar" :class="{'has-secondary-avatar': showAccount && activeAccount.type !== 'user'}">
     <v-avatar
       v-if="showAccount && activeAccount.type === 'user'"
-      class="user-avatar"
+      class="primary-avatar"
       :size="36"
     >
-      <img :src="`${directoryUrl}/api/avatars/user/${user.id}/avatar.png`">
+      <img :src="userAvatarUrl">
     </v-avatar>
     <v-avatar
       v-else
-      class="user-avatar"
+      class="primary-avatar"
       :size="36"
     >
-      <img :src="`${directoryUrl}/api/avatars/${activeAccount.type}/${activeAccount.id}/avatar.png`">
+      <img :src="accountAvatarUrl">
     </v-avatar>
     <v-avatar
-      class="org-avatar"
+      class="secondary-avatar"
       :size="28"
       v-if="showAccount && activeAccount.type !== 'user'"
     >
-      <img :src="`${directoryUrl}/api/avatars/user/${user.id}/avatar.png`">
+      <img :src="userAvatarUrl">
     </v-avatar>
   </div>
 </template>
@@ -33,7 +33,16 @@ export default {
   },
   computed: {
     ...mapState('session', ['directoryUrl', 'user']),
-    ...mapGetters('session', ['activeAccount'])
+    ...mapGetters('session', ['activeAccount']),
+    userAvatarUrl() {
+      return `${this.directoryUrl}/api/avatars/user/${this.user.id}/avatar.png`
+    },
+    accountAvatarUrl() {
+      let url = `${this.directoryUrl}/api/avatars/${this.activeAccount.type}/${this.activeAccount.id}`
+      if (this.activeAccount.department) url += `/${this.activeAccount.department}`
+      url += '/avatar.png'
+      return url
+    }
   },
 }
 </script>
@@ -43,19 +52,19 @@ export default {
   width: 36px;
   text-indent: 0;
 }
-.sd-avatar.has-org-avatar {
+.sd-avatar.has-secondary-avatar {
   width: 56px;
   height: 40px;
   position: relative;
   margin-left: 8px;
   margin-right: 8px;
 }
-.sd-avatar.has-org-avatar .user-avatar {
+.sd-avatar.has-secondary-avatar .primary-avatar {
   position: absolute;
   left: 0;
   top: 0
 }
-.sd-avatar .org-avatar {
+.sd-avatar .secondary-avatar {
   position:absolute;
   right:0px;
   bottom:0;
